f32625f0fa98f7472a7bb3256b5bbf149a1c54a2
WeiJhe <sky820902@gmail.com>
2020-04-12 17:56:57 +0800

    add to load account and password from settings file

--- EarthExplorerDownloader.py
+++ EarthExplorerDownloader.py
@@ -1,8 +1,27 @@
+"""
+1. 讀取帳密
+2. 讀取query.csv
+3. import landsat api and search image name
+4. save image name list to download list
+5. start download images
+"""
 
-class EarthExplorerDownloader():
+import json
+
+class EarthExplorerDownloader(object):
+    SETTINGS_PATH = "./settings.json"
     
+    def __init__(self):
+        with open(self.SETTINGS_PATH) as f:
+            settings = json.load(f)
+
+        self.user_account = settings['user']['account']
+        self.user_password = settings['user']['password']
+
+
     def go(self):
         pass
+
 
 downloader = EarthExplorerDownloader()
 downloader.go()
